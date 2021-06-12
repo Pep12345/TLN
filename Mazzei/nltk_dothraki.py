6557dc81c481b1235420dcf04cf002819c6dc0cd
Giuseppe Biondi <giuv360@hotmail.it>
2021-06-12 16:40:10 +0200

    1

--- Mazzei/nltk_dothraki.py
+++ Mazzei/nltk_dothraki.py
@@ -1,14 +1,13 @@
 from nltk import load_parser
-import nltk
 import os
 
 dir = os.path.dirname(os.path.abspath(__file__))
-text_file = 'file://E:/tln/Mazzei/dothraki_gr_sem.fcfg'
+text_file = 'file://' + dir + '/dothraki_gr_sem.fcfg'
 parser = load_parser(text_file, trace=0)
 sentence = 'Anha zhilak yera'
 sentence2 = 'Anha gavork'
 sentence3 = 'Hash yer astoe ki Dothraki'
-tokens = sentence3.split()
+tokens = sentence.split()
 for tree in parser.parse(tokens):
     print("Label: "+str(tree.label()['SEM'])+"\n")
     print(tree)
